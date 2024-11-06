2ae649053000a28274fd0bea9894ead753e53317
AlessioConsolo <alessioconsolo3@gmail.com>
2024-11-06 17:34:41 +0100

    [UPD] Modified router.js, added routes

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,19 +1,19 @@
 <script>
 export default {
-    props: ["restaurant"]
+  props: ["restaurant"],
 };
 </script>
 
 <template>
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">{{ restaurant.name }}</h5>
-            <p class="card-text">{{ restaurant.type }}</p>
-            <router-link :to="`/menu/${restaurant.id}`" class="btn btn-primary">Vedi Menù</router-link>
-        </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">{{ restaurant.name }}</h5>
+      <p class="card-text">{{ restaurant.type }}</p>
+      <router-link :to="`/menu/${restaurant.id}`" class="btn btn-primary"
+        >Vedi Menù</router-link
+      >
     </div>
+  </div>
 </template>
 
-<style >
-    
-</style>
+<style></style>
