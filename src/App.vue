f4fb8ac1bd369d77913ab66cb9d5564782300a61
eltonanit <elton.ani@gmail.com>
2024-11-06 11:18:42 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  
+}
+</script>
+
+<template>
+  <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="text-center text-primary"> DELIVEBOO</h1>
+        </div>
+        <div class="col-12">
+        </div>
+      </div>    
+  </div>
+</template>
+
+
+<style lang="scss">
+@import './styles/general.scss'
+  
+</style>
